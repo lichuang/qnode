aceff81cb526d6d53ba55e6d2da266716dc4f1bd
lichuang <lichuang@debian.(none)>
2012-11-29 00:21:50 +0800

    delete wrong files

--- src/qnode.h
+++
@@ -1,8 +0,0 @@
-/*
- ** qnode(cute node) -- C + Lua + Actor Model = Erlang-like system
- ** See Copyright Notice at the end of this file
- * */
-#ifndef __QNODE_H__
-#define __QNODE_H__
-
-#endif  /* __QNODE_H__ */
