3341e30a6d8c199f8bcf9cba62807a28fc2e45f7
lichuang <lichuang1982@gmail.com>
2012-12-08 13:55:16 +0800

    add atomic

---
+++ src/qatomic.c
@@ -0,0 +1,25 @@
+/*
+ * See Copyright Notice in qnode.h
+ */
+
+#include "qatomic.h"
+
+void* qnode_atomic_ptr_xchg(void *ptr, void *val) {
+  void *old;
+  __asm__ volatile (
+      "lock; xchg %0, %2"
+      : "=r" (old), "=m" (ptr)
+      : "m" (ptr), "0" (val));
+  return old;
+}
+
+void* qnode_atomic_ptr_cas(void *ptr, void *cmp, void *val) {
+  void *old;
+  __asm__ volatile (
+      "lock; cmpxchg %2, %3"
+      : "=a" (old), "=m" (ptr)
+      : "r" (val), "m" (ptr), "0" (cmp)
+      : "cc");
+  return old;
+}
+
---
+++ src/qatomic.h
@@ -0,0 +1,11 @@
+/*
+ * See Copyright Notice in qnode.h
+ */
+
+#ifndef __QATOMIC_H__
+#define __QATOMIC_H__
+
+void* qnode_atomic_ptr_xchg(void *ptr, void *val);
+void* qnode_atomic_ptr_cas(void *ptr,  void *cmp, void *val);
+
+#endif  /* __QATOMIC_H__ */
