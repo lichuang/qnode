e9c45b8c7d00dc340b237bc23fcae363eff4910f
lichuang <lichuang1982@gmail.com>
2013-01-14 14:46:06 +0800

    use new C api

--- script/server.lua
+++ script/server.lua
@@ -1,8 +1,14 @@
 local server = {}
 
+function accept()
+  print("in accept")
+end
+
 server.start = function()
   print("server start");
-  spawn("child", "test", {id = 2000});
+  --local aid = qspawn("child", "test", {id = 2000});
+  --send(aid, {id = "send"});
+  qlisten(22880, "127.0.0.1", accept);
 end
 
 _G["server"] = server
