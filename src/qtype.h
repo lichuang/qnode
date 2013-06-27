87de39b81c05920a4134e8d3d5d7520415914ea3
lichuang <lichuang1982@gmail.com>
2013-06-27 17:06:36 +0800

    add id macro

--- src/qtype.h
+++ src/qtype.h
@@ -5,17 +5,8 @@
 #ifndef __QTYPE_H__
 #define __QTYPE_H__
 
-typedef char*         qptr_t;
-typedef unsigned int  qid_t;
-
-/*
- * id lowest represent thread id
- * */
-static const int   kMaxThreadBit = 10;
-
-static const qid_t kMaxThreadId = 1 << kMaxThreadBit;
-
-static const qid_t kMaxId = 0x7fffffff & (~kMaxThreadId)
+typedef char*               qptr_t;
+typedef unsigned long long  qid_t;
 
 #define QINVALID_ID 0
 
--- src/qworker.h
+++ src/qworker.h
@@ -13,6 +13,24 @@
 #include "qmsg.h"
 #include "qtype.h"
 
+#define MAX_BIT sizeof(qid_t)
+#define ID_BIT  16
+#define PID_BIT (MAX_BIT - ID_BIT)
+#define POS_ID  PID_BIT
+
+#define MAX_PID (1 << PID_BIT)
+#define MAX_ID  (1 << ID_BIT)
+
+/* creates a mask with `n' 1 bits at position `p' */
+#define MASK1(n,p)  ((~((~(qid_t)0) << n)) << p)
+
+/* creates a mask with `n' 0 bits at position `p' */
+#define MASK0(n,p)  (~MASK1(n,p))
+
+#define make_id(id, pid) (((id) << ID_BIT) | (pid))
+#define get_id(id)       (((id) & MASK1(ID_BIT, POS_ID)) >> ID_BIT)
+#define get_pid(id)      ((id) & MASK1(PID_BIT, 0))
+
 struct qworker_t {
   qmailbox_t            box;
   qhandler_manager_t    manager;
