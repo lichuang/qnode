9072c0e71d35ef479159947fb2fcddf3d07afdfb
lichuang <lichuang1982@gmail.com>
2013-08-14 18:00:05 +0800

    refactor API

--- src/qapi_debug.c
+++ src/qapi_debug.c
@@ -9,17 +9,17 @@
 #include "ldb.h"
 #endif
 
-static int qldebug(lua_State *state);
+static int qlbreak(lua_State *state);
 
 luaL_Reg debug_apis[] = {
-  {"qldebug",          qldebug},
+  {"qlbreak",          qlbreak},
   {NULL, NULL},
 };
 
 static int
-qldebug(lua_State *state) {
+qlbreak(lua_State *state) {
 #ifdef DEBUG  
-  ldb_step_in(state, 1);
+  ldb_break(state);
 #else
   UNUSED(state);
   qerror("debug API disabled, try define DEBUG and recompile");
